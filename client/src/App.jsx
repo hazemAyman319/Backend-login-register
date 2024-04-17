b69362aec6409c9a8e980dd45c446b63020203ec
Hazem <hazemhelmy193@gmail.com>
2024-04-18 00:03:31 +0200

    new-update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-=======
 import './App.css'
 import { Routes,Route } from 'react-router-dom';
 import Navbar from '../src/components/Navbar';
@@ -45,13 +13,7 @@ function App() {
       <Route path='/registers' element={<Registers />}/>
       <Route path='/login' element={<Login />}/>
      </Routes>
->>>>>>> 54df03c3b7ea9e50fa9363cce064cd39ce1cca5e
     </>
   )
 }
-
-<<<<<<< HEAD
-export default App
-=======
 export default App;
->>>>>>> 54df03c3b7ea9e50fa9363cce064cd39ce1cca5e
--- client/src/pages/Registers.jsx
+++ client/src/pages/Registers.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Registers() {
   return (
-    <div>Registers</div>
+    <div>Register</div>
   )
 }
