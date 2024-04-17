54df03c3b7ea9e50fa9363cce064cd39ce1cca5e
Shahd-Mohamed <shahdmohamed.2020812@gmail.com>
2024-04-17 20:26:12 +0200

    new

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import { Routes,Route } from 'react-router-dom';
+import Navbar from '../src/components/Navbar';
+import Home from '../src/pages/Home';
+import Registers from './pages/Registers';
+import Login from'./pages/Login';
 function App() {
-
   return (
     <>
-     <h1>Hello</h1>
+     <Navbar/>
+     <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/registers' element={<Registers />}/>
+      <Route path='/login' element={<Login />}/>
+     </Routes>
     </>
   )
 }
 
-export default App
+export default App;
