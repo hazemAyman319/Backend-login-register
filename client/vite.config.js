7d98db9eb1371f77a7e6af0b72658628c0241260
Hazem <hazemhelmy193@gmail.com>
2024-04-17 20:44:37 +0200

    new update

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// vite.config.js
-export default {
-  server: {
-    port: 5173
-  }
-}
+export default defineConfig({
+  plugins: [react()],
+})
